48a54a7e6aa18955456496f0a8dc2105c54dd067
matthew5025 <matthew5025@users.noreply.github.com>
2019-12-30 09:52:35 +0800

    Added CEPAS Read

--- nfclink/Source/headers/cepas.h
+++ nfclink/Source/headers/cepas.h
@@ -12,25 +12,40 @@
 
 #endif /* NFC_CEPAS_H_ */
 
-typedef union cepas{
-    uint8_t version;
-    uint8_t purseStatus;
-    uint8_t purseBalance[3];
-    uint8_t autoloadAmt[3];
-    uint64_t can;
-    uint64_t csn;
-    uint16_t purseExp;
-    uint16_t purseCreate;
-    uint32_t lastCreditTrp;
-    uint64_t lastCreditHeader;
-    uint8_t numTrans;
-    uint8_t IssuerDataLen;
-    uint32_t lastTransTRP;
-    uint64_t lastTransHeader;
-    uint16_t issuerDataPtr;
+typedef union {
+     struct cepas{
+        uint8_t version;
+        uint8_t purseStatus;
+        uint8_t purseBalance[3];
+        uint8_t autoloadAmt[3];
+        uint8_t can[8];
+        uint8_t csn[8];
+        uint8_t purseExp[2];
+        uint8_t purseCreate[2];
+        uint8_t lastCreditTrp[4];
+        uint8_t lastCreditHeader[8];
+        uint8_t numTrans;
+        uint8_t IssuerDataLen;
+        uint8_t lastTransTRP[4];
+        uint8_t lastTransRec[16];
+        void * issuerDataPtr;
+    }purse;
 }CEPAS;
 
-CEPAS card;
 
-extern void initCEPAS();
-extern void processCommand(uint8_t * pui8RxPayload , uint8_t ui8CmdLength);
+typedef enum
+{
+    WRONG_LENGTH = 0x6700,
+    COMMAND_SUCCESS = 0x9000,
+    COND_NOT_SASTFD = 0x6985
+}apduResp;
+
+
+uint8_t response[256];
+apduResp responseCode;
+uint8_t resLength;
+
+extern CEPAS* initCEPAS();
+extern void processCommand(CEPAS* card, uint8_t * pui8RxPayload , uint8_t ui8CmdLength);
+extern void getResponse(uint8_t * pui8PduBufferPtr);
+extern uint8_t getResponseLength();
